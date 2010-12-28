7ba326bf11f3bb3f8942109476859e7c448e450d
pholz <p.holzkorn@gmail.com>
2010-12-28 21:33:54 +0100

    1st

---
+++ xcode/Resources.h
@@ -0,0 +1,14 @@
+/*
+ *  Resources.h
+ *  kinectBasic
+ *
+ *  Created by Peter Holzkorn on 27/12/2010.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define RES_VERT_ID				CINDER_RESOURCE( ../resources/, mainVert.glsl,	128, GLSL )
+#define RES_FRAG_ID				CINDER_RESOURCE( ../resources/, mainFrag.glsl,	129, GLSL )
